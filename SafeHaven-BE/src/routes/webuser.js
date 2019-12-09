659ff8c16dc03ff3a9f4ced0fd14a2d9e71097d9
Lovisgod <olifedayo94@gmail.com>
2019-12-09 23:30:09 +0100

    FT/user can add sos

--- SafeHaven-BE/src/routes/webuser.js
+++ SafeHaven-BE/src/routes/webuser.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import WebAuthController from '../controllers/WebAuthController';
+import WebContactController from '../controllers/webContactController';
 import auth from '../middleWares/auth';
 const webRouter = express.Router();
 
 webRouter.post('/web/signup', WebAuthController.register);
 webRouter.post('/web/login', WebAuthController.login);
 webRouter.get('/web/profile', auth, WebAuthController.me);
+webRouter.post('/web/add-SOS-contact', auth, WebContactController.addSosContact);
+webRouter.delete('web/delete-sos-contact', auth, WebContactController.addSosContact);
+webRouter.put('/web/update-sos-contact', auth, WebContactController.editSosContact);
 
 
 module.exports = webRouter;
